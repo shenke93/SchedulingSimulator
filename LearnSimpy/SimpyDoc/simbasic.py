4985777c404b9f935b3957909180cf8b8a83657e
KESHEN <shen.ke1993@gmail.com>
2018-07-31 11:07:39 +0200

    Clear ancient files, make it ready for new version using salabim

--- LearnSimpy/SimpyDoc/simbasic.py
+++
@@ -1,8 +0,0 @@
-import simpy
-def example(env):
-    value = yield env.timeout(1, value=42)
-    print('now=%d, value=%d' % (env.now, value))
-    
-env = simpy.Environment()
-p = env.process(example(env))
-env.run()
