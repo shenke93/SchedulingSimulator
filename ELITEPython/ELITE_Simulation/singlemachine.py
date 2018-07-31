ef599f5808b4f8a2d613830ff6c6a3a3fafd6f72
KESHEN <shen.ke1993@gmail.com>
2018-07-31 15:19:17 +0200

    Add energy price, workable demo.

--- ELITEPython/ELITE_Simulation/singlemachine.py
+++ ELITEPython/ELITE_Simulation/singlemachine.py
@@ -5,7 +5,7 @@ class Group(sim.Component):
     Machines in the same group are parallel machines. 
     Attributes: name, idle_machine list, job list, job_select_method
     '''
-    def setup(self, job_select_method, number_of_machines, fraction):
+    def setup(self, job_select_method, number_of_machines, fraction, file_name):
         '''
         Initialize with scheduling strategy, machine numbers
         ''' 
@@ -17,7 +17,9 @@ class Group(sim.Component):
         
         self.fraction = fraction
         
-        self.machines = [Machine(group=self, name=self.name()+'.') for _ in range(number_of_machines)]
+        self.config_file = file_name
+        
+        self.machines = [Machine(group=self, name=self.name()+' Machine.', config_file=self.config_file) for _ in range(number_of_machines)]
 
         self.idle_machines = sim.Queue(self.name() + '.idle_machines')
         
@@ -34,9 +36,12 @@ class Machine(sim.Component):
     Machine is the unit who processes jobs.
     Attributes: group, current running task     
     '''
-    def setup(self, group):
+    def setup(self, group, config_file):
         self.group = group
         self.task = None # Current task running on machine
+        self.energy_cost = 0
+        self.current_energy_price = 0
+        self.config_file = config_file
         
     def process(self):
         while True:
@@ -52,9 +57,13 @@ class Machine(sim.Component):
             job.leave(self.group.jobs)
             self.task = job.tasks.pop()
             self.task.enter(job.task_in_execution)
+            self.update_price()
             yield self.hold(self.task.duration)
+            # Calculate energy cost
+            self.energy_cost += self.task.duration * self.current_energy_price
+#             print(self.name)
+#             print(self.energy_cost)
             self.task.leave(job.task_in_execution)
-            
             if job.tasks:   # Even if one task of job is done, there are remaining tasks to do
                 task1 = job.tasks.head()
                 job.enter(task1.group.jobs) # Add the job to to-do-job list of the group
@@ -63,7 +72,18 @@ class Machine(sim.Component):
             else:
                 job.leave(plant) # All tasks of job is finished, the job can leave the plant
 
-
+    def update_price(self):
+        with open(self.config_file, encoding='utf-8') as file:
+            for line in file:
+                key, value = line.split(',', 2)
+                time = int(key)
+                price = int(value)
+                if (env.now() < time):
+                    break
+                t = price
+        self.current_energy_price = t
+            
+        
 class JobGenerator(sim.Component):
     '''
     JobGenerator is used to generate jobs (from file)
@@ -112,7 +132,7 @@ class Task(sim.Component):
     Attributes:group, duration
     '''
     def setup(self, job_generator, job):
-        self.group = job_generator.group_dist()
+        self.group = job_generator.group_dist.sample()
         self.duration = job_generator.duration_dist
         
 
@@ -131,26 +151,19 @@ with sim.ItemFile('config.txt') as f:
         number_of_machines = f.read_item_int()
         fraction = f.read_item_float()
         groups.append(Group(name = name, job_select_method = job_select_method, 
-                            number_of_machines = number_of_machines, fraction = fraction))
+                            number_of_machines = number_of_machines, fraction = fraction, file_name = 'price.txt'))
 
 plant = sim.Queue('plant') # Job list of the plant        
 
-# bug with reading things from file! Do not use package format!
-# with open('jobInfo.csv', encoding='utf-8') as csvfile:
-#     for line in csvfile:
-#         inter_arrival_time_dist, number_of_tasks_dist, group_dist, duration_dist = line.split(',', 4)
-#         print(inter_arrival_time_dist)
-#         print(number_of_tasks_dist)
-#         print(group_dist)
-#         print(duration_dist)
-
 group_dist = sim.Pdf(groups, probabilities=[group.fraction for group in groups])
 
-JobGenerator(file_name = 'JobInfo.csv', group_dist = group_dist)
+JobGenerator(file_name = 'JobInfo.txt', group_dist = group_dist)
         
 env.run(200)
-        
-        
+
+print(groups[0].machines[0].energy_cost)        
+# plant.print_statistics()    
+# plant.print_info()    
         
 
 
